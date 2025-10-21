ea45cb477baa8d2083c72ae855d39bbe4b77218e
bcs23090010-prog <bcs23090010@student.uts.edu.my>
2025-10-21 20:37:59 +0800

    Add files via upload

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "LabTask2UI")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "LabTask2UI.Pages")]
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LabTask2UI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
